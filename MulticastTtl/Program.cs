b358d4e02d5a4bc5ca3083660905ac92f90c34d8
Pawel Gerr <pawel.gerr@thinktecture.com>
2018-05-18 09:10:35 +0200

    Initial commit

---
+++ MulticastTtl/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace MulticastTtl
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// var addressFamily = AddressFamily.InterNetwork;
+			var addressFamily = AddressFamily.InterNetworkV6;
+
+			var socket = new Socket(addressFamily, SocketType.Dgram, ProtocolType.Udp);
+
+			socket.SetSocketOption(addressFamily == AddressFamily.InterNetwork ? SocketOptionLevel.IP : SocketOptionLevel.IPv6, SocketOptionName.MulticastTimeToLive, 32);
+		}
+	}
+}
